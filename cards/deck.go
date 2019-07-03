d791627c819428c25d1cd0ee431bbc5d70214f27
sajicode <sajioloye@gmail.com>
2019-07-03 11:20:28 +0100

    setup method to create new decks

--- cards/deck.go
+++ cards/deck.go
@@ -6,6 +6,25 @@ import "fmt"
 // below, we are creating a type deck which is like a slice of strings
 type deck []string
 
+// the function below returns a value of type deck (slice of strings)
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	// to populate the deck with all possible combinations, we do a nested loop to combine cardSuits & cardValues
+
+	// replace unused values with "_"
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 // function below is a receiver function
 func (d deck) print() {
 	// any variable of type "deck" gets access to the "print" method
--- cards/main.go
+++ cards/main.go
@@ -2,13 +2,8 @@ package main
 
 func main() {
 
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of spades")
+	cards := newDeck()
 
 	// because "cards" is of type "deck", it gets access to the "print" method
 	cards.print()
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
