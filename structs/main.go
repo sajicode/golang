19ea01fa6fad5165d66c84cbdfcfad5026543621
sajicode <sajioloye@gmail.com>
2019-07-09 08:50:32 +0100

    update structs; log properties & values

--- structs/main.go
+++ structs/main.go
@@ -8,7 +8,17 @@ type person struct {
 }
 
 func main() {
+	// declaring a struct
+	// *1
 	// alex := person{"Alex", "Anderson"}
-	alex := person{firstName: "Alex", lastName: "Anderson"}
+	// *2
+	// alex := person{firstName: "Alex", lastName: "Anderson"}
+	// *3 The ininialized variable comes back as an empty struct
+	var alex person
+
+	alex.firstName = "Alex"
+	alex.lastName = "Morgan"
 	fmt.Println(alex)
+	// * Below logs the fields and their values
+	fmt.Printf("%+v", alex)
 }
