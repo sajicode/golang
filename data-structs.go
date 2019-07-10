ac9f5bcb4233e3a85ec1e97c876c83e6e6fbf770
sajicode <sajioloye@gmail.com>
2019-07-10 14:53:58 +0100

    iterate with for loop

--- data-structs.go
+++ data-structs.go
@@ -10,7 +10,15 @@ func newSlice() slices {
 	return slice
 }
 
+//* merge two slices
 func (d slices) mergeSlice(slice1 []int, slice2 []int) {
 
 	fmt.Printf("%v\n", append(slice1, slice2...))
+}
+
+//* iterating over a slice with a for loop from the 2nd element
+func (d slices) iterateSlice() {
+	for index := 1; index < len(d); index++ {
+		fmt.Printf("Index: %d Value: %d\n", index, d[index])
+	}
 }
--- main.go
+++ main.go
@@ -5,10 +5,15 @@ import "fmt"
 func main() {
 	fmt.Println("Go there>>>")
 
-	s1 := []int{1, 2, 3}
-	s2 := []int{4, 5, 6}
+	// s1 := []int{1, 2, 3}
+	// s2 := []int{4, 5, 6}
 
-	slice1 := newSlice()
+	// slice1 := newSlice()
 
-	slice1.mergeSlice(s1, s2)
+	// slice1.mergeSlice(s1, s2)
+	slice := newSlice()
+
+	slice = append(slice, 4, 5, 6, 7, 8)
+
+	slice.iterateSlice()
 }
