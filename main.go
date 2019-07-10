7a0ce620d2003a32046913aff1c2062b9b90d876
sajicode <sajioloye@gmail.com>
2019-07-10 15:21:32 +0100

    multidimensional arrays

--- main.go
+++ main.go
@@ -11,9 +11,14 @@ func main() {
 	// slice1 := newSlice()
 
 	// slice1.mergeSlice(s1, s2)
-	slice := newSlice()
 
-	slice = append(slice, 4, 5, 6, 7, 8)
+	// slice := newSlice()
 
-	slice.iterateSlice()
+	// slice = append(slice, 4, 5, 6, 7, 8)
+
+	// slice.iterateSlice()
+	//* multidimensional slices => two slices within a slice
+	multiSlice := [][]int{{10}, {100, 200}}
+	multiSlice[0] = append(multiSlice[0], 20)
+	fmt.Println(multiSlice)
 }
