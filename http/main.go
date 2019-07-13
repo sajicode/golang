6d80685e57f3c0cb67157669bad7ac63c3892333
sajicode <sajioloye@gmail.com>
2019-07-13 22:17:00 +0100

    utilize Read func to log google homepage

--- http/main.go
+++ http/main.go
@@ -14,5 +14,12 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(resp)
+	//* create a byte slice that would hold all the data passed by the Read function
+	bs := make([]byte, 99999)
+	//* the 2nd parameter (99999) specifies the capacity? of the byte slice
+	//* the reason we set up our byte slice with make & 99999 is bcos the Read function is not set up to resize the size of the slice if full
+	//* the response Body has access to the Read interface
+	resp.Body.Read(bs)
+	fmt.Println(string(bs))
+
 }
