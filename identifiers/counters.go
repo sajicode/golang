81e69dae455e5228af24e71973af28fdbe35c794
sajicode <sajioloye@gmail.com>
2019-07-18 19:29:39 +0100

    isolate packages by unexporting them

---
+++ identifiers/counters.go
@@ -0,0 +1,8 @@
+//* this way, the contents of this package cannot be accessed outside
+
+//* when an identifier starts with a lowercase e.g alertCounter, the
+//* identifier is unexported or unknown to code outside the package
+
+package counters
+
+type alertCounter int
---
+++ identifiers/main.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	// create a variable of the unexported type and initialize the value to 10
+	counter := counters.alertCounter(10)
+}
