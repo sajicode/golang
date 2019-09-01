cf4c19aa2cf61fb8d4cbc459525c89da030c8963
sajicode <sajioloye@gmail.com>
2019-09-01 16:04:06 +0100

    word counter without locks

---
+++ goroutines/race.go
@@ -0,0 +1,68 @@
+//* word counter with race condition
+package main
+
+
+
+func main() {
+	var wg sync.WaitGroup
+
+	w := newWords()
+
+	for _, f := range os.Args[1:] {
+		wg.Add(1)
+		go func(file string) {
+			if err := tallyWords(file, w); err != nil {
+				fmt.Println(err.Error())
+			}
+			wg.Done()
+		}(f)
+	}
+	wg.Wait()
+
+	//* at the end of the program, we print what we find
+	fmt.Println("Words that appear more than once:")
+	for word, count := range w.found {
+		if count > 1 {
+			fmt.Printf("%s: %d\n", word, count)
+		}
+	}
+}
+
+//* we track words in a struct
+type words struct {
+	found map[string]int
+}
+
+//* create a new word instance
+func newWords() *words {
+	return &words(found: map[string]int{})
+}
+
+//* Tracks the number of times we have seen a word
+func (w *words) add(word string, n int) {
+	//* if the word isn't already tracked, add it, else increment count
+	count, ok := w.found[word]
+	if !ok {
+		w.found[word] = n
+		return
+	}
+	w.found[word] = count + n
+}
+
+//* open a file, parse its contents, and count the words that appear.
+func tallyWords(filename string, dict *words) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	//* scanner is a useful tool for parsing files like this
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanWords)
+	for scanner.Scan() {
+		word := strings.ToLower(scanner.Text())
+		dict.add(word, 1)
+	}
+	return scanner.Err()
+}
